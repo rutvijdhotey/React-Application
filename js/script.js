ae3f7ffb85aaeb427e6283b00c5d60dd01a94f05
rutvijdhotey <rutvij.dhotey@gmail.com/Users/rutvijdhotey/Documents/Sofwares/apache-ant-1.9.6/bin/antRun>
2016-08-30 14:39:39 -0400

    halfway there commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+var Note =  React.createClass({
+	render: function() {
+		return (
+			<div className = "note"> </div>
+			);
+	}
+});
+
+ReactDOM.render(<Note> hello </Note>,document.getElementById('react-container')); 
